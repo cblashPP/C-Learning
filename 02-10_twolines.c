cfdf1198f00928d4320b51f46c52845dd234f04e
Mr. Conrad <62264897+cblashPP@users.noreply.github.com>
2022-12-27 10:59:56 -0800

    Exercise 02_10
    Deals with using the escape operator '\' for adding quotes to output as well as adding newline characters

---
+++ 02-10_twolines.c
@@ -0,0 +1,14 @@
+//
+//  02-10_twolines.c
+//  first_test
+//
+//  Created by Conrad Blash on 12/27/22.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("This is \"not\"\na Jojo reference\n");
+    return (5);
+}
