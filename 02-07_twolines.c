3ff8e6fea4c7c250c90a450619f123bf9305d834
Mr. Conrad <62264897+cblashPP@users.noreply.github.com>
2022-12-27 10:48:49 -0800

    Exercise 02_07
    Deals with using the escape operator '\' to manipulate lines in the printf function and in strings in general.

---
+++ 02-07_twolines.c
@@ -0,0 +1,16 @@
+/*
+ This code is brought to you by your honest and reliable hardware engineer,
+ Journeyer to the east and to the west,
+ Alumnus of the majestic black mecca,
+ Lover of wretched automatons of our daily life!
+ The one, the only Conrad Blash
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("If at first you don't succeed....\nTry, try again.\n");
+    return (0);
+}
+
